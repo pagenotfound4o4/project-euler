e3f297bf45f2672e459116e4879f87904242c04f
Unlucky <unlucky1990@gmail.com>
2015-11-23 14:24:25 +0800

    add 018, 067

---
+++ 067/solver.py
@@ -0,0 +1,15 @@
+#coding: utf-8
+
+arr, dp = [], []
+with open('p067_triangle.txt', 'r') as fin:
+    lines = [line.rstrip("\r\n") for line in fin.readlines()]
+for line in lines:
+    arr.append([int(item) for item in line.split()])
+    dp.append([0] * len(arr[-1]))
+n = len(arr)
+for i in range(n):
+    dp[n-1][i] = arr[n-1][i]
+for i in range(n-2, -1, -1):
+    for j in range(i+1):
+        dp[i][j] = max(dp[i+1][j], dp[i+1][j+1]) + arr[i][j]
+print(dp[0][0])
